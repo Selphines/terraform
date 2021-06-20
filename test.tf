9775a9e231c87bb38b8bfa89d9dd4f5d309aea7a
Selhines <apelsin.mx@gmail.com>
2021-06-20 13:31:53 +0300

    New Commit

--- test.tf
+++ test.tf
@@ -1,1 +1,21 @@
-/Users/maxim/Documents/dev/AWS/terraform/test.tf
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+
+/*
+resource "aws_instance" "my_ubuntu" {
+  ami           = "ami-0194c3e07668a7e36 "
+  instance_type = "t3.micro"
+}
+*/
