fdaa42536009ea95c0d68479575d9cfd136cf89b
Selphines <apelsin.mx@gmail.com>
2021-06-20 19:53:42 +0300

    Add Web.sh

--- web.sh
+++ web.sh
@@ -3,4 +3,4 @@ apt install apache2 -y
 myip=$(curl checkip.amazonaws.com)
 echo "<h2>WebServer with my IP: $myip</h2><br>Build by Terraform!" > /var/www/html/index.html
 sudo service apache2 start
-chekconfig apache2 on
+chkconfig apache2 on
